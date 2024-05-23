c13ed9a3565a3919f78f203abe06d2b74c441e06
David Kamundi <119663577+dkamundi@users.noreply.github.com>
2024-05-23 22:54:08 +0300

    archive

--- Archive/tempCodeRunnerFile.js
+++
@@ -1,8 +0,0 @@
-transporter.sendMail(mailOptions, function(error, info) {
-        if (error) {
-            console.log('Error sending email:', error);
-        } else {
-            console.log('Email sent:', info.response);
-        }
-    });
-}
